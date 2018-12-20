3b0471676017d69c49921cc38d64d91dd444690c
henry_chou <hendry.js1247@gmail.com>
2018-12-20 11:19:19 +0800

    Update test case and refactoring parser

--- NLogLab.Tests/UnitTest1.cs
+++ NLogLab.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace NLogLab.Tests
         [Test]
         public void DynamicLog()
         {
-            string json = "{\"array\":[{\"name\":\"henry\",\"age\":25},{\"name\":\"henry\",\"age\":25},{\"name\":\"henry\",\"age\":25}],\"array2\":[1,2,3],\"array3\":[[1,2,3],[1,2,3],[1,2,3]],\"boolean\":true,\"color\":\"#82b92c\",\"null\":null,\"number\":123,\"object\":{\"a\":\"b\",\"c\":\"d\",\"e\":\"f\"},\"string\":\"Hello World\"}";
+            string json = "{\"array\":[{\"color\":\"henry\",\"age\":25},{\"color\":\"henry\",\"age\":25},{\"color\":\"henry\",\"age\":25}],\"array2\":[1,2,3],\"array3\":[[1,2,3],[1,2,3],[1,2,3]],\"boolean\":true,\"color\":\"#82b92c\",\"null\":null,\"number\":123,\"object\":{\"color\":\"b\",\"c\":\"d\",\"e\":\"f\"},\"string\":\"Hello World\"}";
             NLogMain main = new NLogMain();
             main.DemoDynamicWrite(json);
         }
--- NLogLab/NLogMain.cs
+++ NLogLab/NLogMain.cs
@@ -1,12 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NLog;
-using NLog.Config;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
 
 namespace NLogLab
 {
@@ -15,61 +10,51 @@ namespace NLogLab
         public ILogger Logger { get; set; }
         public NLogMain()
         {
-            var oldJsonConverter = NLog.Config.ConfigurationItemFactory.Default.JsonConverter;
-            var newConverter = new JsonNetSerializer(oldJsonConverter);
-            ConfigurationItemFactory.Default.JsonConverter = newConverter;
             Logger = LogManager.GetLogger("MyLogger");
         }
         public void DemoDynamicWrite(string json)
         {
-            IDictionary<string, object> result = deserializeToDictionary(json);
+            IDictionary<string, object> result = DeserializeToObjectDictionary(json);
             Logger.Info("Response {@Response}", result);
         }
 
-        private Dictionary<string, object> deserializeToDictionary(string jo)
+        private Dictionary<string, object> DeserializeToObjectDictionary(string json)
         {
-            var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(jo);
-            var values2 = new Dictionary<string, object>();
-            foreach (KeyValuePair<string, object> d in values)
+            var dictNode = new Dictionary<string, object>();
+            foreach (KeyValuePair<string, object> pair in
+                 JsonConvert.DeserializeObject<Dictionary<string, object>>(json))
             {
-                // if (d.Value.GetType().FullName.Contains("Newtonsoft.Json.Linq.JObject"))
-                if (d.Value is JObject)
-                {
-                    values2.Add(d.Key, deserializeToDictionary(d.Value.ToString()));
-                }
-                else if (d.Value is JArray)
-                {
-                    values2.Add(d.Key, deserializeToListDictionary(d.Value.ToString()));
-                }
-                else
-                {
-                    values2.Add(d.Key, d.Value);
-                }
+                dictNode.Add(pair.Key, ParseJsonObject(pair.Value));
             }
-            return values2;
+            return dictNode;
         }
 
-        private List<object> deserializeToListDictionary(string json)
+        private object ParseJsonObject(object jsonObejct)
         {
-            var values = JsonConvert.DeserializeObject<List<object>>(json);
-            var values2 = new List<object>();
-            foreach (object d in values)
+            if (jsonObejct is JObject)
             {
-                // if (d.Value.GetType().FullName.Contains("Newtonsoft.Json.Linq.JObject"))
-                if (d is JObject)
-                {
-                    values2.Add(deserializeToDictionary(d.ToString()));
-                }
-                else if (d is JArray)
-                {
-                    values2.Add(deserializeToListDictionary(d.ToString()));
-                }
-                else
-                {
-                    values2.Add(d);
-                }
+                return DeserializeToObjectDictionary(jsonObejct.ToString());
+
             }
-            return values2;
+            else if (jsonObejct is JArray)
+            {
+                return DeserializeToObjectList(jsonObejct.ToString());
+            }
+            else
+            {
+                return jsonObejct;
+            }
+        }
+
+        private List<object> DeserializeToObjectList(string json)
+        {
+            var listNode = new List<object>();
+            foreach (object node in
+                JsonConvert.DeserializeObject<List<object>>(json))
+            {
+                listNode.Add(ParseJsonObject(node));
+            }
+            return listNode;
         }
     }
 
